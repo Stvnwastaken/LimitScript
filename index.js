ee27e7934021d6ca54ab64540e72c9fd8c8abeb5
LightLord <87213208+LightLordYT@users.noreply.github.com>
2022-12-09 22:01:16 -0800

    password integration

--- index.js
+++ index.js
@@ -1,30 +1,60 @@
+// ==UserScript==
+// @name         LimitScript
+// @namespace    http://github.com/LightLordYT
+// @version      1.2.3
+// @description  a simple script that allows you to block certain websites with @match after x time elapsed, password implemented and can be changed along with time elapsed to block
+// @author       LightLord
+// @match        https://crazygames.com/*
+// @match        https://poki.com/*
+// @match        https://*.io/
+// @match        https://*.one/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// ==/UserScript==
+
+//put urls u want to block in //@match
+
 (function(window, document) {
-    let initObject = {
-        init: () => {
-          let date = new Date()
-          let time = 0
-          let old_date = localStorage.getItem("Light's_date")
-          if(old_date !== date){
+  let initObject = {
+      handleRes: (name) => {
+        function res(){
+          let response = prompt('pswrd?')
+          if(response == 'focusboy'){
             localStorage.setItem("Light's_tracker", 0)
+            alert('refresh the page')
+          }else{
+            alert('wrong boy')
           }
-          let dateTracker = localStorage.setItem("Light's_date", date)
-          let isTracker = localStorage.getItem("Light's_tracker") ? true : false
-          isTracker ? '' : localStorage.setItem("Light's_tracker", 0)
-          },
-        update: setInterval(() => {
-            let name = window.location.href
-            console.log(name)
-            let tracker = localStorage.getItem("Light's_tracker")
-            tracker++
-            console.log(tracker)
-            tracker > 30 ? document.body.innerHTML = `<h1 style='color: black; font-weight: semi-bold; text-align: center;'>You have exceeded the time limit of playing: ${name} </h1>` : ''
-            localStorage.setItem("Light's_tracker", tracker)
-        }, 5000)
-    }
-        window.onLoad = () => {
-            let currentObject = initObject
-            currentObject.init()
-            currentObject.update()
         }
+        document.body.innerHTML = `<h1 style='background-color: white; color: black; font-weight: semi-bold; text-align: center;'>You have exceeded the time limit of playing: ${name} </h1>`
+        document.addEventListener('keydown', (e) => {
+            //bind whatever key you want
+          e.key == 'Control' ? res() : ''
+        })
+      },
+      init: () => {
+        let date = new Date()
+        let old_date = localStorage.getItem("Light's_date")
+        if(old_date !== date){
+          localStorage.setItem("Light's_tracker", 0)
+        }
+        localStorage.setItem("Light's_date", date)
+        let isTracker = localStorage.getItem("Light's_tracker") ? true : false
+        isTracker ? '' : localStorage.setItem("Light's_tracker", 0)
+        },
+      update: setInterval(() => {
+          let handler = initObject.handleRes.bind(initObject)
+          let name = window.location.href
+          let tracker = localStorage.getItem("Light's_tracker")
+          tracker++
+          tracker > 720 ? handler(name) : ''
+          localStorage.setItem("Light's_tracker", tracker)
+      }, 5000)
+  }
+      window.onLoad = () => {
+          let currentObject = initObject
+          currentObject.init()
+          currentObject.update()
+      }
 
 })(window, document);
